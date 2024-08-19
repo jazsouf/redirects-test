548f94a80c690252953aa1245fb571faf9e67a02
jazsouf <101473962+jazsouf@users.noreply.github.com>
2024-08-19 16:02:19 +0300

    init

---
+++ src/app/index/page.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="grid place-items-center h-full w-full">Index</div>
+      </main>
+    </main>
+  );
+}
