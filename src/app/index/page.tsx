ae2dceb8e7b1e72bf79d8c0d69467a4456d1b24d
jazsouf <101473962+jazsouf@users.noreply.github.com>
2024-08-19 16:34:36 +0300

    build error for /index

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,4 +1,4 @@
-export default function Index() {
+export default function IndexPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
